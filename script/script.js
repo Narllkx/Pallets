e937c26d30ef8bdc74d668199c56dde0fda7bade
Narllkx <narllkx@gmail.com>
2023-04-07 17:11:18 +0500

    Типо законченый support секция но ещё хз

--- script/script.js
+++ script/script.js
@@ -14,3 +14,21 @@ $(".slider-box").slick({
   autoplaySpeed: 2000,
 });
 /* Slider */
+
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.querySelector("#sub").value = "Отправка...";
+  let data = new FormData(form);
+  fetch(
+    "https://docs.google.com/spreadsheets/d/1PVRZWMerbTAO9aebm-lvtD_cezQxHwMAUGrhRoclVak/edit#gid=0",
+    {
+      method: "POST",
+      body: data,
+    }
+  )
+    .then((res) => res.text())
+    .then((data) => {
+      document.querySelector("#sub").value = "Отправлено!";
+    });
+});
